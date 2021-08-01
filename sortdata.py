3cd7d2eaadbf7649eca6b90aeeff94d245edaaf0
withoutBlink <yeluoqiulin@gmail.com>
2021-08-02 01:31:36 +0800

    update python data analysing tools

--- sortdata.py
+++ sortdata.py
@@ -1,12 +1,20 @@
 # This Python file uses the following encoding: utf-8
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
-file = open('./testdata.txt')
-newfile = open('./sortdata.txt')
+#newfile = open('./sortdata.txt')
 datamap = []
-lines = file.readlines()
+lines = open('./testdata.txt','r').readlines()
+
 for line in lines:
     datamap.append(line[1:]+":"+line[:1])
 datamap.sort()
+
 for line in datamap:
     [second,first] = line.split(":")
     newfile.write(first+second)
+
+
+
+fig = plt.figure()

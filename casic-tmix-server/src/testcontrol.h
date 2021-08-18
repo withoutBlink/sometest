0800043b76ae5cdc446d226cfc8935a3072e9bac
ht706-qiulin <yeluoqiulin@gmail.com>
2021-08-18 13:49:10 +0800

    update some comments

--- casic-tmix-server/src/testcontrol.h
+++ casic-tmix-server/src/testcontrol.h
@@ -30,7 +30,7 @@ extern "C"  {
 
 
 class TestItem
-{
+{ // determine test results and status internal logic
 public:
     TestItem(IDINT id);
     // ~TestItem();
@@ -47,21 +47,21 @@ public:
 private:
     RWLock _Lock;
     IDINT _test_id;// same as document described, MAC as id
-    size_t _test_type;
+    size_t _test_type;// define wether the procedure is interactive, and determine the item is belong to function test group or performance group
     std::string _test_name;
     std::string _test_key;
     std::string _test_info;
     std::string _test_standard;
-    std::string _test_start_prog;
-    std::string _test_stop_prog;
-    bool _test_select = false;
-    bool _Result = false;// variable
-    bool _Repaired = false;// variable
-    size_t _Status;//ready=0, running=1, finished=2, error=3, vairable
+    std::string _test_start_prog;// cmd to start test
+    std::string _test_stop_prog;// some how useless, because we use some brutal way to do it
+    bool _test_select = true;// some how useless, because if some TestItem instance has been created which means it had been selected already.
+    bool _Result = false;// pass or not pass
+    bool _Repaired = false;// variable only exists in mem not db
+    size_t _Status;// ready=0, running=1, finished=2, error=3, vairable
 };
 
 class TargetDev
-{
+{ // establish connection between test items and test device, manage test result upload to db
 public:
     std::string GetMac();
     std::string GetIP();
@@ -73,7 +73,6 @@ public:
     std::vector<TestItem> GetErrlist();
 
     TargetDev(std::string mac, std::string ipaddr);
-    ~TargetDev();
 
 private:
     std::vector<TestItem> InitItemlist();
@@ -87,25 +86,23 @@ private:
 };
 
 class TestControl
-{
+{ // manage the whole test procedure
 public:
     static TestControl *Instance();
 	static void Destory();
 
     std::string GetSystime(); // send system time to client for time settings
-    bool Prepare(std::string ipaddr, nlohmann::json content); // start specific machine test process
-    nlohmann::json Start(std::string ipaddr);
+    bool Prepare(std::string ipaddr, nlohmann::json content);// preparation before start test
+    nlohmann::json Start(std::string ipaddr);// start specific machine test process
     bool SetStarted(std::string ipaddr, nlohmann::json content);// set test status 1
-    void Stop(std::string ipaddr);// stop specific machine test process
-    float Status(std::string ipaddr);// check test complete ratio
-    nlohmann::json ItemList();
-    nlohmann::json ItemResults(std::string ipaddr);// get specific machine test result from database
-    bool SetItemResult(std::string ipaddr, const nlohmann::json content);
-    bool Reload();
-
+    void Stop(std::string ipaddr);// stop specific device test process
+    float Status(std::string ipaddr);// check test complete ratio, TODO
+    nlohmann::json ItemList();// return a minimal version of test list, used for task distribution
+    nlohmann::json ItemResults(std::string ipaddr);// get specific device test result from database
+    bool SetItemResult(std::string ipaddr, const nlohmann::json content);// process result uploaded by client
+    bool Reload(std::string ipaddr);// reload test process for specific device
 
     //For program testing
-
 
 private:
     TestControl();
@@ -114,8 +111,8 @@ private:
     bool isFresh(std::string ipaddr);// get test list itemconf from database
     nlohmann::json FreshStart(std::string ipaddr);// fresh start a new test
     nlohmann::json Resume(std::string ipaddr);// start from last saved
-    bool CheckReady(nlohmann::json content, std::string ipaddr);// check client machine if it is ready
-    void SetItemConf(nlohmann::json conf);
+    bool CheckReady(nlohmann::json content, std::string ipaddr);// check client if it is ready
+    void SetItemConf(nlohmann::json conf);// reverted interface for init test list according to messages from web
     void AddDev(TargetDev dev);// bind connection remote ip address with MAC address, write to _MACbind
     void ReleaseDev(TargetDev dev);// release binding after test error or interuption
 
